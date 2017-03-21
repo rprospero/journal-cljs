ba07a25a7aa73459abe695b270cd22ead9b78f2f
Washington <adam.washington@stfc.ac.uk>
2017-03-21 13:21:42 +0000

    Basic xml navigation and display

--- build.boot
+++ build.boot
@@ -1,6 +1,9 @@
 (set-env!
  :source-paths #{"src/cljs"}
  :resource-paths #{"html"}
- :dependencies '[[adzerk/boot-cljs "1.7.228-2"]])
+ :dependencies '[[adzerk/boot-cljs "1.7.228-2"]
+                 [funcool/tubax "0.2.0"]
+                 [reagent "0.6.1"]
+                 [traversy "0.5.0"]])
 
 (require '[adzerk.boot-cljs :refer [cljs]])
--- src/cljs/journal_cljs/core.cljs
+++ src/cljs/journal_cljs/core.cljs
@@ -1,5 +1,51 @@
-(ns journal-cljs.core)
+(ns journal-cljs.core
+  (:require [reagent.core :as r]
+            [traversy.lens :refer [view in each *> conditionally]]
+            [tubax.core :as xml]))
+(def xml "<rss version=\"2.0\">
+           <channel>
+             <title>RSS Title</title>
+             <description>This is an example of an RSS feed</description>
+             <link>http://www.example.com/main.html</link>
+             <lastBuildDate>Mon, 06 Sep 2010 00:01:00 +0000 </lastBuildDate>
+             <pubDate>Sun, 06 Sep 2009 16:20:00 +0000</pubDate>
+             <ttl>1800</ttl>
+             <item>
+               <title>Example entry</title>
+               <description>Here is some text containing an interesting description.</description>
+               <link>http://www.example.com/blog/post/1</link>
+               <guid isPermaLink=\"false\">7bd204c6-1655-4c27-aeee-53f933c5395f</guid>
+               <pubDate>Sun, 06 Sep 2009 16:20:00 +0000</pubDate>
+             </item>
+             <item>
+               <title>Example entry2</title>
+               <description>Here is some text containing an interesting description.</description>
+               <link>http://www.example.com/blog/post/1</link>
+               <guid isPermaLink=\"false\">7bd204c6-1655-4c27-aeee-53f933c5395f</guid>
+               <pubDate>Sun, 06 Sep 2009 16:20:00 +0000</pubDate>
+             </item>
+           </channel>
+         </rss>")
+
+(defn simple-component [message]
+  [:div
+   [:p (str message)]])
+
+(defn render-simple [message]
+  (r/render
+   [simple-component message]
+   (.getElementById js/document "root")))
+
+(defn in-tag [tag]
+  (conditionally #(= tag (:tag %))))
 
 (enable-console-print!)
 
-(print "Hello, World!")
+(render-simple
+ (->
+  (xml/xml->clj xml)
+  (view (*> (in [:content])
+            each (in [:content])
+            each (in-tag :item) (in [:content])
+            each (in-tag :title) (in [:content])
+            each))))
